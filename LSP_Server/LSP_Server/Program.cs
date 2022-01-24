c0434bba4d1241fa84fa6d696a387c2b4b52ed3f
david weinstein <david.weinstein@buildertrend.com>
2022-01-24 14:55:45 -0600

    Initial Commit

---
+++ LSP_Server/LSP_Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using OmniSharp.Extensions.LanguageServer.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace LSP_Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => MainAsync(args).Wait();
+
+        private static async Task MainAsync(string[] args)
+        {
+
+            var server = await LanguageServer.From(
+                options =>
+                    options
+                       .WithInput(Console.OpenStandardInput())
+                       .WithOutput(Console.OpenStandardOutput())
+            ).ConfigureAwait(false);
+
+            await server.WaitForExit.ConfigureAwait(false);
+        }
+    }
+}

6f0a89b7104d348a22903a71059925e63e195a6f
david weinstein <david.weinstein@buildertrend.com>
2022-01-26 10:44:26 -0600

    Basic Server

--- LSP_Server/LSP_Server/Handlers/HoverHandler.cs
+++ LSP_Server/LSP_Server/Handlers/HoverHandler.cs
@@ -1,11 +1,6 @@
-﻿using LSP_Server.Workspace;
-using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,17 +8,6 @@ namespace LSP_Server.Handlers
 {
     public class HoverHandler : IHoverHandler
     {
-        private readonly DocumentsManager _documentsManager;
-        private readonly DictionaryManager _dictionaryManager;
-        private readonly PositionManager _positionManager;
-
-        public HoverHandler(DocumentsManager documentsManager, DictionaryManager dictionaryManager, PositionManager positionManager)
-        {
-            _documentsManager = documentsManager;
-            _dictionaryManager = dictionaryManager;
-            _positionManager = positionManager;
-        }
-
         public HoverRegistrationOptions GetRegistrationOptions(HoverCapability capability, ClientCapabilities clientCapabilities)
         {
             return new HoverRegistrationOptions()
@@ -43,27 +27,10 @@ namespace LSP_Server.Handlers
 
         public Task<Hover> Handle(HoverParams request, CancellationToken cancellationToken)
         {
-            var document = _documentsManager.GetDocument(request.TextDocument.Uri.ToString());
-            var wordAtPos = _positionManager.GetWordAtPosition(document, request.Position);
-            var definitions = _dictionaryManager.GetDefinitions(wordAtPos);
-
-            try
+            return Task.FromResult(new Hover()
             {
-                return Task.FromResult(new Hover()
-                {
-                    Contents = new MarkedStringsOrMarkupContent(definitions.Select(d => new MarkedString(d)))
-                });
-            }
-            catch
-            {
-                var sb = new StringBuilder();
-                sb.AppendLine("word: " + wordAtPos);
-                foreach(var def in definitions)
-                {
-                    sb.AppendLine(def);
-                }
-                throw new Exception(sb.ToString());
-            }
+                Contents = new MarkedStringsOrMarkupContent("This is hover text")
+            });
         }
     }
 }
--- LSP_Server/LSP_Server/Program.cs
+++ LSP_Server/LSP_Server/Program.cs
@@ -1,15 +1,8 @@
 ﻿using LSP_Server.Handlers;
-using LSP_Server.Workspace;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Server;
-using Serilog;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace LSP_Server
@@ -18,24 +11,12 @@ namespace LSP_Server
     {
         private static async Task Main(string[] args)
         {
-            //Log.Logger = new LoggerConfiguration()
-            //    .Enrich.FromLogContext()
-            //    .WriteTo.File("log.txt")
-            //    .MinimumLevel.Verbose()
-            //    .CreateLogger();
-
             var server = await LanguageServer.From(
                 options =>
                     options
                        .WithInput(Console.OpenStandardInput())
                        .WithOutput(Console.OpenStandardOutput())
-                       .ConfigureLogging(x => x.AddSerilog(Log.Logger)
-                            .AddLanguageProtocolLogging()
-                            .SetMinimumLevel(LogLevel.Trace)
-                        )
-                       .WithHandler<TextDocumentSyncHandler>()
                        .WithHandler<HoverHandler>()
-                       .WithHandler<ReferencesHandler>()
                        .WithServices(
                             services =>
                             {
@@ -45,31 +26,8 @@ namespace LSP_Server
                                         Section = "plaintext"
                                     }
                                 );
-                                services.AddSingleton<DocumentsManager>();
-                                services.AddSingleton<DictionaryManager>();
-                                services.AddSingleton<PositionManager>();
                             }
                        )
-                       .OnInitialize(
-                            async (server, request, token) =>
-                            {
-                                var documentsManager = server.Services.GetService<DocumentsManager>();
-                                var folderPaths = new List<string>();
-
-                                if (request.Capabilities.Workspace.WorkspaceFolders)
-                                {
-                                    folderPaths.AddRange(request.WorkspaceFolders?.Select(f => f.Uri.GetFileSystemPath()) ?? new List<string>());
-                                }
-
-                                if (folderPaths == null)
-                                {
-                                    folderPaths = request.RootUri == null ? new List<string>() { request.RootUri.GetFileSystemPath() } : new List<string>();
-                                }
-
-                                documentsManager.InitializeWorkspace(folderPaths);
-                            }
-                       )
-
             );
 
             await server.WaitForExit;
